c7b848fa3b310a11f315994dc0f350598ad3d927
Alex Gamble <agamble.uk@gmail.com>
2024-07-25 14:07:43 +0100

    Yet another fix

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,12 +1,13 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin "marked" # @13.0.2
-pin "dompurify" # @3.1.6
-pin "dropzone" # @6.0.0
-pin "just-extend" # @5.1.1
-pin "@rails/activestorage", to: "@rails--activestorage.js" # @7.1.3
+pin 'application'
+pin '@hotwired/turbo-rails', to: 'turbo.min.js'
+pin '@hotwired/stimulus', to: 'stimulus.min.js'
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
+pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin 'marked' # @13.0.2
+pin 'dompurify' # @3.1.6
+pin 'dropzone' # @6.0.0
+pin 'just-extend' # @5.1.1
+pin '@rails/activestorage', to: '@rails--activestorage.js' # @7.1.3
+pin_all_from 'app/javascript/helpers', under: 'helpers'
