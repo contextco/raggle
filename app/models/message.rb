c6d2cf6e68bd81c612c2079cd2349b95216cfe68
Alex Gamble <agamble.uk@gmail.com>
2024-07-25 17:56:31 +0100

    Small fixes

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,6 @@ class Message < ApplicationRecord
   attribute :content, :string, default: ''
 
   has_many_attached :files
+
+  enum role: %w[user assistant system].index_by(&:to_sym), _suffix: true
 end
