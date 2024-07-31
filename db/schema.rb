d8828030affd36ac39d489aeead1f9434f193fd0
Amisha <amisha@context.ai>
2024-07-31 16:32:04 +0100

    Schema lintter fix

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/BlockLength
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,211 +14,213 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_31_100343) do
+ActiveRecord::Schema[7.1].define(version: 20_240_731_100_343) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "active_storage_attachments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.uuid "record_id", null: false
-    t.uuid "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  create_table 'active_storage_attachments', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'record_type', null: false
+    t.uuid 'record_id', null: false
+    t.uuid 'blob_id', null: false
+    t.datetime 'created_at', null: false
+    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
+    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness', unique: true
   end
 
-  create_table "active_storage_blobs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  create_table 'active_storage_blobs', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
+    t.string 'key', null: false
+    t.string 'filename', null: false
+    t.string 'content_type'
+    t.text 'metadata'
+    t.string 'service_name', null: false
+    t.bigint 'byte_size', null: false
+    t.string 'checksum'
+    t.datetime 'created_at', null: false
+    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
   end
 
-  create_table "active_storage_variant_records", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  create_table 'active_storage_variant_records', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
+    t.uuid 'blob_id', null: false
+    t.string 'variation_digest', null: false
+    t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
   end
 
-  create_table "chats", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "model"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.uuid "user_id"
-    t.index ["user_id"], name: "index_chats_on_user_id"
+  create_table 'chats', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
+    t.string 'model'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.uuid 'user_id'
+    t.index ['user_id'], name: 'index_chats_on_user_id'
   end
 
-  create_table "chunks", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "document_id", null: false
-    t.integer "chunk_index", null: false
-    t.text "content", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["document_id", "chunk_index"], name: "index_chunks_on_document_id_and_chunk_index", unique: true
-    t.index ["document_id"], name: "index_chunks_on_document_id"
+  create_table 'chunks', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
+    t.uuid 'document_id', null: false
+    t.integer 'chunk_index', null: false
+    t.text 'content', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index %w[document_id chunk_index], name: 'index_chunks_on_document_id_and_chunk_index', unique: true
+    t.index ['document_id'], name: 'index_chunks_on_document_id'
   end
 
-  create_table "documents", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "message_id", null: false
-    t.string "documentable_type"
-    t.integer "documentable_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["documentable_type", "documentable_id"], name: "index_documents_on_documentable_type_and_documentable_id"
-    t.index ["message_id"], name: "index_documents_on_message_id"
+  create_table 'documents', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
+    t.uuid 'message_id', null: false
+    t.string 'documentable_type'
+    t.integer 'documentable_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index %w[documentable_type documentable_id], name: 'index_documents_on_documentable_type_and_documentable_id'
+    t.index ['message_id'], name: 'index_documents_on_message_id'
   end
 
-  create_table "messages", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "role"
-    t.string "content"
-    t.uuid "chat_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["chat_id"], name: "index_messages_on_chat_id"
+  create_table 'messages', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
+    t.string 'role'
+    t.string 'content'
+    t.uuid 'chat_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['chat_id'], name: 'index_messages_on_chat_id'
   end
 
-  create_table "solid_queue_blocked_executions", force: :cascade do |t|
-    t.bigint "job_id", null: false
-    t.string "queue_name", null: false
-    t.integer "priority", default: 0, null: false
-    t.string "concurrency_key", null: false
-    t.datetime "expires_at", null: false
-    t.datetime "created_at", null: false
-    t.index ["concurrency_key", "priority", "job_id"], name: "index_solid_queue_blocked_executions_for_release"
-    t.index ["expires_at", "concurrency_key"], name: "index_solid_queue_blocked_executions_for_maintenance"
-    t.index ["job_id"], name: "index_solid_queue_blocked_executions_on_job_id", unique: true
+  create_table 'solid_queue_blocked_executions', force: :cascade do |t|
+    t.bigint 'job_id', null: false
+    t.string 'queue_name', null: false
+    t.integer 'priority', default: 0, null: false
+    t.string 'concurrency_key', null: false
+    t.datetime 'expires_at', null: false
+    t.datetime 'created_at', null: false
+    t.index %w[concurrency_key priority job_id], name: 'index_solid_queue_blocked_executions_for_release'
+    t.index %w[expires_at concurrency_key], name: 'index_solid_queue_blocked_executions_for_maintenance'
+    t.index ['job_id'], name: 'index_solid_queue_blocked_executions_on_job_id', unique: true
   end
 
-  create_table "solid_queue_claimed_executions", force: :cascade do |t|
-    t.bigint "job_id", null: false
-    t.bigint "process_id"
-    t.datetime "created_at", null: false
-    t.index ["job_id"], name: "index_solid_queue_claimed_executions_on_job_id", unique: true
-    t.index ["process_id", "job_id"], name: "index_solid_queue_claimed_executions_on_process_id_and_job_id"
+  create_table 'solid_queue_claimed_executions', force: :cascade do |t|
+    t.bigint 'job_id', null: false
+    t.bigint 'process_id'
+    t.datetime 'created_at', null: false
+    t.index ['job_id'], name: 'index_solid_queue_claimed_executions_on_job_id', unique: true
+    t.index %w[process_id job_id], name: 'index_solid_queue_claimed_executions_on_process_id_and_job_id'
   end
 
-  create_table "solid_queue_failed_executions", force: :cascade do |t|
-    t.bigint "job_id", null: false
-    t.text "error"
-    t.datetime "created_at", null: false
-    t.index ["job_id"], name: "index_solid_queue_failed_executions_on_job_id", unique: true
+  create_table 'solid_queue_failed_executions', force: :cascade do |t|
+    t.bigint 'job_id', null: false
+    t.text 'error'
+    t.datetime 'created_at', null: false
+    t.index ['job_id'], name: 'index_solid_queue_failed_executions_on_job_id', unique: true
   end
 
-  create_table "solid_queue_jobs", force: :cascade do |t|
-    t.string "queue_name", null: false
-    t.string "class_name", null: false
-    t.text "arguments"
-    t.integer "priority", default: 0, null: false
-    t.string "active_job_id"
-    t.datetime "scheduled_at"
-    t.datetime "finished_at"
-    t.string "concurrency_key"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["active_job_id"], name: "index_solid_queue_jobs_on_active_job_id"
-    t.index ["class_name"], name: "index_solid_queue_jobs_on_class_name"
-    t.index ["finished_at"], name: "index_solid_queue_jobs_on_finished_at"
-    t.index ["queue_name", "finished_at"], name: "index_solid_queue_jobs_for_filtering"
-    t.index ["scheduled_at", "finished_at"], name: "index_solid_queue_jobs_for_alerting"
+  create_table 'solid_queue_jobs', force: :cascade do |t|
+    t.string 'queue_name', null: false
+    t.string 'class_name', null: false
+    t.text 'arguments'
+    t.integer 'priority', default: 0, null: false
+    t.string 'active_job_id'
+    t.datetime 'scheduled_at'
+    t.datetime 'finished_at'
+    t.string 'concurrency_key'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['active_job_id'], name: 'index_solid_queue_jobs_on_active_job_id'
+    t.index ['class_name'], name: 'index_solid_queue_jobs_on_class_name'
+    t.index ['finished_at'], name: 'index_solid_queue_jobs_on_finished_at'
+    t.index %w[queue_name finished_at], name: 'index_solid_queue_jobs_for_filtering'
+    t.index %w[scheduled_at finished_at], name: 'index_solid_queue_jobs_for_alerting'
   end
 
-  create_table "solid_queue_pauses", force: :cascade do |t|
-    t.string "queue_name", null: false
-    t.datetime "created_at", null: false
-    t.index ["queue_name"], name: "index_solid_queue_pauses_on_queue_name", unique: true
+  create_table 'solid_queue_pauses', force: :cascade do |t|
+    t.string 'queue_name', null: false
+    t.datetime 'created_at', null: false
+    t.index ['queue_name'], name: 'index_solid_queue_pauses_on_queue_name', unique: true
   end
 
-  create_table "solid_queue_processes", force: :cascade do |t|
-    t.string "kind", null: false
-    t.datetime "last_heartbeat_at", null: false
-    t.bigint "supervisor_id"
-    t.integer "pid", null: false
-    t.string "hostname"
-    t.text "metadata"
-    t.datetime "created_at", null: false
-    t.index ["last_heartbeat_at"], name: "index_solid_queue_processes_on_last_heartbeat_at"
-    t.index ["supervisor_id"], name: "index_solid_queue_processes_on_supervisor_id"
+  create_table 'solid_queue_processes', force: :cascade do |t|
+    t.string 'kind', null: false
+    t.datetime 'last_heartbeat_at', null: false
+    t.bigint 'supervisor_id'
+    t.integer 'pid', null: false
+    t.string 'hostname'
+    t.text 'metadata'
+    t.datetime 'created_at', null: false
+    t.index ['last_heartbeat_at'], name: 'index_solid_queue_processes_on_last_heartbeat_at'
+    t.index ['supervisor_id'], name: 'index_solid_queue_processes_on_supervisor_id'
   end
 
-  create_table "solid_queue_ready_executions", force: :cascade do |t|
-    t.bigint "job_id", null: false
-    t.string "queue_name", null: false
-    t.integer "priority", default: 0, null: false
-    t.datetime "created_at", null: false
-    t.index ["job_id"], name: "index_solid_queue_ready_executions_on_job_id", unique: true
-    t.index ["priority", "job_id"], name: "index_solid_queue_poll_all"
-    t.index ["queue_name", "priority", "job_id"], name: "index_solid_queue_poll_by_queue"
+  create_table 'solid_queue_ready_executions', force: :cascade do |t|
+    t.bigint 'job_id', null: false
+    t.string 'queue_name', null: false
+    t.integer 'priority', default: 0, null: false
+    t.datetime 'created_at', null: false
+    t.index ['job_id'], name: 'index_solid_queue_ready_executions_on_job_id', unique: true
+    t.index %w[priority job_id], name: 'index_solid_queue_poll_all'
+    t.index %w[queue_name priority job_id], name: 'index_solid_queue_poll_by_queue'
   end
 
-  create_table "solid_queue_recurring_executions", force: :cascade do |t|
-    t.bigint "job_id", null: false
-    t.string "task_key", null: false
-    t.datetime "run_at", null: false
-    t.datetime "created_at", null: false
-    t.index ["job_id"], name: "index_solid_queue_recurring_executions_on_job_id", unique: true
-    t.index ["task_key", "run_at"], name: "index_solid_queue_recurring_executions_on_task_key_and_run_at", unique: true
+  create_table 'solid_queue_recurring_executions', force: :cascade do |t|
+    t.bigint 'job_id', null: false
+    t.string 'task_key', null: false
+    t.datetime 'run_at', null: false
+    t.datetime 'created_at', null: false
+    t.index ['job_id'], name: 'index_solid_queue_recurring_executions_on_job_id', unique: true
+    t.index %w[task_key run_at], name: 'index_solid_queue_recurring_executions_on_task_key_and_run_at', unique: true
   end
 
-  create_table "solid_queue_scheduled_executions", force: :cascade do |t|
-    t.bigint "job_id", null: false
-    t.string "queue_name", null: false
-    t.integer "priority", default: 0, null: false
-    t.datetime "scheduled_at", null: false
-    t.datetime "created_at", null: false
-    t.index ["job_id"], name: "index_solid_queue_scheduled_executions_on_job_id", unique: true
-    t.index ["scheduled_at", "priority", "job_id"], name: "index_solid_queue_dispatch_all"
+  create_table 'solid_queue_scheduled_executions', force: :cascade do |t|
+    t.bigint 'job_id', null: false
+    t.string 'queue_name', null: false
+    t.integer 'priority', default: 0, null: false
+    t.datetime 'scheduled_at', null: false
+    t.datetime 'created_at', null: false
+    t.index ['job_id'], name: 'index_solid_queue_scheduled_executions_on_job_id', unique: true
+    t.index %w[scheduled_at priority job_id], name: 'index_solid_queue_dispatch_all'
   end
 
-  create_table "solid_queue_semaphores", force: :cascade do |t|
-    t.string "key", null: false
-    t.integer "value", default: 1, null: false
-    t.datetime "expires_at", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["expires_at"], name: "index_solid_queue_semaphores_on_expires_at"
-    t.index ["key", "value"], name: "index_solid_queue_semaphores_on_key_and_value"
-    t.index ["key"], name: "index_solid_queue_semaphores_on_key", unique: true
+  create_table 'solid_queue_semaphores', force: :cascade do |t|
+    t.string 'key', null: false
+    t.integer 'value', default: 1, null: false
+    t.datetime 'expires_at', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['expires_at'], name: 'index_solid_queue_semaphores_on_expires_at'
+    t.index %w[key value], name: 'index_solid_queue_semaphores_on_key_and_value'
+    t.index ['key'], name: 'index_solid_queue_semaphores_on_key', unique: true
   end
 
-  create_table "teams", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'teams', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "uploaded_files", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'uploaded_files', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "email", null: false
-    t.string "name"
-    t.string "profile_picture_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.uuid "team_id"
-    t.string "encrypted_password"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["team_id"], name: "index_users_on_team_id"
+  create_table 'users', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
+    t.string 'email', null: false
+    t.string 'name'
+    t.string 'profile_picture_url'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.uuid 'team_id'
+    t.string 'encrypted_password'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['team_id'], name: 'index_users_on_team_id'
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "chats", "users"
-  add_foreign_key "chunks", "documents"
-  add_foreign_key "documents", "messages"
-  add_foreign_key "messages", "chats"
-  add_foreign_key "solid_queue_blocked_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
-  add_foreign_key "solid_queue_claimed_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
-  add_foreign_key "solid_queue_failed_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
-  add_foreign_key "solid_queue_ready_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
-  add_foreign_key "solid_queue_recurring_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
-  add_foreign_key "solid_queue_scheduled_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
-  add_foreign_key "users", "teams"
+  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'chats', 'users'
+  add_foreign_key 'chunks', 'documents'
+  add_foreign_key 'documents', 'messages'
+  add_foreign_key 'messages', 'chats'
+  add_foreign_key 'solid_queue_blocked_executions', 'solid_queue_jobs', column: 'job_id', on_delete: :cascade
+  add_foreign_key 'solid_queue_claimed_executions', 'solid_queue_jobs', column: 'job_id', on_delete: :cascade
+  add_foreign_key 'solid_queue_failed_executions', 'solid_queue_jobs', column: 'job_id', on_delete: :cascade
+  add_foreign_key 'solid_queue_ready_executions', 'solid_queue_jobs', column: 'job_id', on_delete: :cascade
+  add_foreign_key 'solid_queue_recurring_executions', 'solid_queue_jobs', column: 'job_id', on_delete: :cascade
+  add_foreign_key 'solid_queue_scheduled_executions', 'solid_queue_jobs', column: 'job_id', on_delete: :cascade
+  add_foreign_key 'users', 'teams'
 end
+
+# rubocop:enable Metrics/BlockLength
