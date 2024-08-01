534d41c4a1fd2172546b373a446e906b0262b6b4
Amisha <amisha@context.ai>
2024-08-01 11:54:09 +0100

    Comment resolved

--- spec/factories/uploaded_file_factory.rb
+++ spec/factories/uploaded_file_factory.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :uploaded_file do
-    # Adding this line to suppress rubocop warning about empty block
-  end
+  factory :uploaded_file
 end
