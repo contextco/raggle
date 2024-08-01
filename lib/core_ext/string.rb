f83f1c9c89a2f65dc602baa55313b6e3febcdc2b
Amisha <amisha@context.ai>
2024-08-01 12:21:05 +0100

    Fix

--- lib/core_ext/string.rb
+++ lib/core_ext/string.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
-class String
-  def each_chunk(chunk_size)
-    return enum_for(:each_chunk, chunk_size) unless block_given?
+# Define the CoreExt module
+module CoreExt
+  # Define the String module within CoreExt
+  module String
+    def each_chunk(chunk_size)
+      return enum_for(:each_chunk, chunk_size) unless block_given?
 
-    (0...length).step(chunk_size) do |start_idx|
-      yield self[start_idx, chunk_size]
+      (0...length).step(chunk_size) do |start_idx|
+        yield self[start_idx, chunk_size]
+      end
     end
   end
+end
+
+class ::String
+  include CoreExt::String
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,6 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
-require_relative '../lib/core_ext/string'
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
