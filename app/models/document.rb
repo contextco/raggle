2d5a9b697a39ba0564779ed20c19539c7862440c
Amisha <150023807+amisha29sh@users.noreply.github.com>
2024-08-01 15:09:37 +0100

    Document chunking done with overlap (#42)

--- app/models/document.rb
+++ app/models/document.rb
@@ -10,7 +10,8 @@ class Document < ApplicationRecord
 
   after_commit :chunk_attachment, on: %i[create update]
 
-  CHUNK_SIZE = 2_000
+  CHUNK_SIZE = 512
+  CHUNK_OVERLAP = 32
 
   private
 
@@ -18,7 +19,7 @@ class Document < ApplicationRecord
     return unless attachment.attached? && chunks.empty?
 
     content = attachment.download
-    content.each_chunk(CHUNK_SIZE).with_index do |chunk_content, chunk_index|
+    content.each_chunk(CHUNK_SIZE, CHUNK_OVERLAP).with_index do |chunk_content, chunk_index|
       chunks.create!(chunk_index:, content: chunk_content)
     end
   end
