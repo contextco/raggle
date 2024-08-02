fadbace31bdcb4caebd86be645ef78439ab51b3a
Alex Gamble <agamble.uk@gmail.com>
2024-08-02 15:00:09 +0100

    Add an 'ownership' table to represent relationships between users and documents
    Multiple users could share access to the same underlying documents, hence the many-to-many relationship here.
    
    This will also allow us to further restrict the scope of the vector search, since we can look solely at documents that a user has access to.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_02_105243) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_02_134734) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "vector"
@@ -192,6 +192,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_02_105243) do
   create_table "uploaded_files", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_document_ownerships", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "document_id", null: false
+    t.index ["document_id"], name: "index_user_document_ownerships_on_document_id"
+    t.index ["user_id"], name: "index_user_document_ownerships_on_user_id"
   end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
