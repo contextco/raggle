2ac8397599544a1f0c1f7d02a9462cf2227db2f3
Alex Gamble <agamble.uk@gmail.com>
2024-08-02 16:00:38 +0100

    Move chunking of attachments into uploaded file since that logic is specific to that implementation.

--- app/models/chunk.rb
+++ app/models/chunk.rb
@@ -11,7 +11,20 @@ class Chunk < ApplicationRecord
 
   before_create :generate_embedding
 
+DEFAULT_SIZE = 512
+DEFAULT_OVERLAP = 32
+
+
   private
+  
+  
+  
+  def self.from_string!(content)
+  content.each_chunk(DEFAULT_SIZE, DEFAULT_OVERLAP)
+  .with_index do |chunk_content, chunk_index|
+  chunks.create!(chunk_index:, content: chunk_content)
+end
+end
 
   def generate_embedding
     self.embedding = EmbeddingService.generate(content)
