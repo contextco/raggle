4ec83cc0c8fbc8bc18a5ddad4effa536c8f435e7
Alex Gamble <agamble.uk@gmail.com>
2024-08-06 17:09:05 +0100

    Add ability to sync google docs into documents/chunks

---
+++ db/migrate/20240801160529_add_google_oauth_to_users.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddGoogleOauthToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :google_oauth, :jsonb
+  end
+end
--- db/migrate/20240802134734_add_join_table_between_users_and_documents.rb
+++ db/migrate/20240802134734_add_join_table_between_users_and_documents.rb
@@ -2,9 +2,10 @@
 
 class AddJoinTableBetweenUsersAndDocuments < ActiveRecord::Migration[7.1]
   def change
-    create_join_table :users, :documents, table_name: :user_document_ownerships do |t|
+    create_join_table :users, :documents, table_name: :user_document_ownerships, column_options: { type: :uuid } do |t|
       t.index :user_id
       t.index :document_id
+      t.index %i[user_id document_id], unique: true
     end
   end
 end
