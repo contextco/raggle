cd45b543e7e9681c39b7e7ca26c2d1e8c3f2f133
Alex Gamble <agamble@users.noreply.github.com>
2024-08-09 16:27:17 +0100

    Begin to decouple the process of signing in from assigning permissions to integrations. (#69)
    This PR adds a new settings page where you can connect your Google Drive. This means we don't need to request this permission on login, plus we now have a place for other integrations to request their permissions.
    
    In the next PR I'll kick off the sync job after the permissions have been granted here.

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, ENV.fetch('GOOGLE_CLIENT_ID', nil), ENV.fetch('GOOGLE_CLIENT_SECRET', nil), {
+    scope: [
+      Ingestors::Google::Docs::REQUIRED_SCOPE
+    ].join(' '),
+    name: :google_with_google_drive,
+    include_granted_scopes: true,
+    prompt: 'consent',
+    callback_path: '/_/permissions/google/callback'
+  }
+end
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -6,6 +6,24 @@ FactoryBot.define do
     password { FFaker::Internet.password }
     name { FFaker::Name.name }
     profile_picture_url { FFaker::Internet.uri('https') }
+
+    transient do
+      google_oauth_scopes { nil }
+    end
+
+    google_oauth do
+      {
+        'info' => {
+          'email' => FFaker::Internet.email,
+          'name' => FFaker::Name.name,
+          'image' => FFaker::Internet.uri('https')
+        },
+        'credentials' => {
+          'token' => FFaker::IdentificationMX.rfc,
+          'scope' => (google_oauth_scopes || %w[email profile openid]).join(' ')
+        }
+      }
+    end
     team
   end
 end
