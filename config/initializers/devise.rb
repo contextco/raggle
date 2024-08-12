ef15357b159faf8aed30b42d1670bb718924a4f2
Alex Gamble <agamble.uk@gmail.com>
2024-08-12 11:18:45 +0100

    Fix CSRF bug on authentication
    Since we had registered omniauth twice (once with devise and once via the Omniauth initializer), the middleware was running twice on each callback request. This was causing some issues where credentials wouldn't be assigned after sign in.
    
    The unfortunate side effect of this PR is that a user will need to add additional callback URLs in their Google Oauth configuration. Have spent some time looking at this but can't find a way around at the moment.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -310,11 +310,4 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-
-  config.omniauth :google_oauth2, ENV.fetch('GOOGLE_CLIENT_ID', nil), ENV.fetch('GOOGLE_CLIENT_SECRET', nil), {
-    scope: %w[
-      https://www.googleapis.com/auth/userinfo.email
-      https://www.googleapis.com/auth/userinfo.profile
-    ].join(' ')
-  }
 end
