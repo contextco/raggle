82397a3831cf985e03756db715f23e3595a47cbf
Alex Gamble <agamble.uk@gmail.com>
2024-08-13 15:28:45 +0100

    Add gmail message previews in search results.

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe SearchesController, type: :request do
+  include ActionView::RecordIdentifier
+
   let(:user) { create(:user) }
 
   before do
@@ -17,11 +19,16 @@ RSpec.describe SearchesController, type: :request do
 
     context 'when a query is present', vcr: 'controllers/search' do
       let(:google_drive_file) { create(:google_drive_file) }
-      let(:chunk) { create(:chunk, document: google_drive_file.document, content: 'foo') }
+      let(:drive_file_chunk) { create(:chunk, document: google_drive_file.document, content: 'foo') }
+
+      let(:gmail_message) { create(:gmail_message) }
+      let(:gmail_chunk) { create(:chunk, document: gmail_message.document, content: 'foo') }
 
       before do
         user.documents << google_drive_file.document
-        chunk
+        user.documents << gmail_message.document
+        drive_file_chunk
+        gmail_chunk
       end
 
       it 'enqueues a search job' do
@@ -30,9 +37,14 @@ RSpec.describe SearchesController, type: :request do
         end.to have_enqueued_job(PerformSearchJob).with('foo', user, anything)
       end
 
-      it 'renders the results template' do
+      it 'renders the results for the google drive file' do
         get search_path, params: { q: 'foo' }
         expect(response.body).to include(google_drive_file.title)
+      end
+
+      it 'renders the results for the gmail message' do
+        get search_path, params: { q: 'foo' }
+        expect(response.body).to include(dom_id(gmail_message, :result))
       end
     end
   end
