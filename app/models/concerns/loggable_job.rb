395cb6e168878354aeaf838d05a1c36599cd072f
Amisha <amisha@context.ai>
2024-08-13 16:34:42 +0100

    Alex's comments

--- app/models/concerns/loggable_job.rb
+++ app/models/concerns/loggable_job.rb
@@ -4,17 +4,21 @@ module LoggableJob
   extend ActiveSupport::Concern
 
   included do
-    before_perform :start_logging
-    after_perform :end_logging
+    around_perform :log_task
   end
 
   private
 
-  def start_logging
+  def log_task
     @log = SyncLog.start(task_name: self.class.name, user: arguments.first)
+    yield
+    @log.mark_as_completed!
+  rescue StandardError => e
+    mark_completed
+    raise e
   end
 
-  def end_logging
+  def mark_completed
     @log.mark_as_completed!
   end
 end
--- spec/jobs/sync/gmail_messages_job_spec.rb
+++ spec/jobs/sync/gmail_messages_job_spec.rb
@@ -7,27 +7,45 @@ RSpec.describe Sync::GmailMessagesJob, type: :job do
 
   let(:user) { create(:user) }
   let(:gmail_ingestor) { instance_double(Ingestors::Google::Gmail) }
+  let(:sync_log) { instance_double(SyncLog, mark_as_completed!: true) }
 
   before do
     allow(Ingestors::Google::Gmail).to receive(:new).with(user).and_return(gmail_ingestor)
-    allow(gmail_ingestor).to receive(:ingest)
-    allow(SyncLog).to receive(:start).and_return(double('SyncLog', mark_as_completed!: true))
+    allow(SyncLog).to receive(:start).and_return(sync_log)
   end
 
-  it 'performs the job' do
+  it 'performs the job successfully' do
+    allow(gmail_ingestor).to receive(:ingest)
+
     perform_enqueued_jobs do
       Sync::GmailMessagesJob.perform_later(user)
       expect(Ingestors::Google::Gmail).to have_received(:new).with(user)
       expect(gmail_ingestor).to have_received(:ingest)
+      expect(sync_log).to have_received(:mark_as_completed!)
     end
   end
 
-  it 'starts and ends logging' do
+  it 'starts and ends logging successfully' do
+    allow(gmail_ingestor).to receive(:ingest)
+
     perform_enqueued_jobs do
-      expect(SyncLog).to receive(:start).with(task_name: 'Sync::GmailMessagesJob', user:).and_return(double('SyncLog', mark_as_completed!: true))
+      expect(SyncLog).to receive(:start).with(task_name: 'Sync::GmailMessagesJob', user:).and_return(sync_log)
+      expect(sync_log).to receive(:mark_as_completed!)
       Sync::GmailMessagesJob.perform_later(user)
     end
   end
+
+  it 'handles failure and marks log as completed' do
+    allow(gmail_ingestor).to receive(:ingest).and_raise(StandardError.new('Something went wrong'))
+
+    expect do
+      Sync::GmailMessagesJob.perform_now(user)
+    end.to raise_error(StandardError, 'Something went wrong')
+
+    expect(SyncLog).to have_received(:start).with(task_name: 'Sync::GmailMessagesJob', user:)
+    expect(sync_log).to have_received(:mark_as_completed!)
+  end
+
 
   after do
     clear_enqueued_jobs
