614ba32ca7efcd92ea3348e2f7436c52887d5ca0
Alex Gamble <agamble.uk@gmail.com>
2024-08-15 12:27:23 +0100

    Make the root page the search function instead of chat

--- app/components/ui/sidebar_navigation_icon_component.rb
+++ app/components/ui/sidebar_navigation_icon_component.rb
@@ -3,8 +3,11 @@
 class Ui::SidebarNavigationIconComponent < ApplicationComponent
   attribute :icon
   attribute :path
+  attribute :root, default: false
 
   def active?
+    return true if root && request.path == '/'
+
     request.path.start_with?(path)
   end
 end
